decc4c041c67f01d747d8f3f0a40f1e786d1d260
Sikander Sandhu <sikandersandhu82@gmail.com>
2021-12-09 11:50:41 +1100

    Update & inherit from Transaction class heirarchy
    Following methods updated:
    
    - DepositTransaction(Account account, decimal amount) : base(amount)
      calls base contructor to initialize amount.
    
    - override void Execute(); updated to override base virtual Execute
      method and add further functionality.
    
    - override void Rollback(); updated to override base virtual Rollback
      method and add further functionality.
    
    - override void print(); overrides abstract print and provides
      functionality.

--- BankingSystem/BankingSystem/DepositTransaction.cs
+++ BankingSystem/BankingSystem/DepositTransaction.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BankingSystem_Iteration2
+namespace BankingSystem_Iteration3
 {
     /// <summary>
     /// The methods in this class merely call methods from the account class to 
@@ -15,50 +15,27 @@ namespace BankingSystem_Iteration2
     /// - Rollback deposit by calling Withdraw()
     /// - Print account summary by calling ToString()
     /// </summary>
-    class DepositTransaction
+    class DepositTransaction: Transaction
     {
         /// <summary>
         /// This field will be initialized with the account in which the 
         /// deposit is to be made
         /// </summary>
         private Account _account;
+
         /// <summary>
-        /// This field is initialized with the decimal amount to be deposited
-        /// to the account
+        /// Stores the account balance snapshot 
+        /// during the object initialization.
+        /// Initialized in the constructor.
         /// </summary>
-        private decimal _amount;
-        /// <summary>
-        /// Strores the true/false status of the execution of deposit
-        /// </summary>
-        private bool _executed;
-        /// <summary>
-        /// Public property to read the execution status of deposit
-        /// </summary>
-        public bool Executed
-        {
-            get { return _executed; }
-        }
-        /// <summary>
-        /// Stores the true/false status of the success of deposit
-        /// </summary>
-        private bool _success;
+        private readonly decimal _currBalance;
+
         /// <summary>
         /// Public property to read the success status of the deposit
         /// </summary>
-        public bool Success
+        public override bool Success
         {
             get { return _success; }
-        }
-        /// <summary>
-        /// Strores the true/false status of the reversal of deposit transaction
-        /// </summary>
-        private bool _reversed;
-        /// <summary>
-        /// public property to read the reversed status of the deposit 
-        /// </summary>
-        public bool Reversed
-        {
-            get { return _reversed; }
         }
 
         /// <summary>
@@ -67,10 +44,10 @@ namespace BankingSystem_Iteration2
         /// </summary>
         /// <param name="account">The account to deposit amount into</param>
         /// <param name="amount">The amount to be deposited in the account</param>
-        public DepositTransaction(Account account, decimal amount)
+        public DepositTransaction(Account account, decimal amount) : base(amount)
         {
             _account = account;
-            _amount = amount; 
+            _currBalance = _account.Balance;
         }
 
         /// <summary>
@@ -79,11 +56,12 @@ namespace BankingSystem_Iteration2
         /// error messages if the deposit fails.
         /// </summary>
         /// <exception cref="InvalidOperationException">This exceptions is thrown if the deposit failed.</exception>
-        public void Execute()
+        public override void Execute()
         { 
             // If the deposit has not been executed
             if (!Executed)
             {
+                base.Execute();
                 // executes the deposit and store the status               
                 bool succeeded = _account.Deposit(_amount);
 
@@ -91,24 +69,21 @@ namespace BankingSystem_Iteration2
                 if (succeeded)
                 {
                     // changes executed and success status to true
-                    // and prints account summary
-                    _executed = true;
+                    // and prints account summary                   
                     _success = true;
-                    Print();
                 }
-
                 // If deposit failed
                 else
                 {
-                    // prints account summary and throws the
-                    // following error message.
-                    Print();
                     throw new InvalidOperationException("Deposit amount must be greater than zero.");
                 }
-            }  
+            }
             // if the deposit has already been executed,
             // throws the following error message
-            else throw new InvalidOperationException("The Deposit has already been succesfully executed.");
+            else
+            {
+                throw new InvalidOperationException("The Deposit has already been succesfully executed.");
+            }
         }
 
         /// <summary>
@@ -119,22 +94,18 @@ namespace BankingSystem_Iteration2
         /// <exception cref="InvalidOperationException">This exception is thrown if the reversal fails</exception>
         /// <exception cref="InvalidOperationException">This exception is thrown if the reversal has 
         /// already been succuesfully executed.</exception>
-        public void Rollback()
+        public override void Rollback()
         {
             // If the depsoit has not been reversed
             if (!Reversed)
             {
+                base.Rollback();
+
                 //Reverses deposit and stores the status
                 bool succeeded = _account.Withdraw(_amount);
 
                 // If deposit succeeded
-                if (succeeded)
-                {
-                    // changes reverse status to true
-                    _reversed = true;
-                }
-                // otherwise, throws the following exception
-                else throw new InvalidOperationException("Rollback failed. Not enough funds in the acccount to make a withdrawl.");
+                if (!succeeded) throw new InvalidOperationException("Rollback failed. Not enough funds in the acccount to make a withdrawl.");
             }
             // If the deposit has already been reversed, thorws the following exception
             else throw new InvalidOperationException("The amount has already been succesfully rolledback.");
@@ -143,16 +114,18 @@ namespace BankingSystem_Iteration2
         /// <summary>
         /// Prints the success or failed status of the deposit attempt and prints the account summary
         /// </summary>
-        public void Print()
+        public override void Print()
         {
             if (Success)
             {
-                Console.WriteLine("\n***** Deposit successful *****\n");
-                Console.WriteLine(_account.ToString());
+                Console.WriteLine("\t***** Account Summary *****\n");
+                Console.WriteLine("** {0} ** Deposit successful **\n", DateStamp.ToString());
+                Console.WriteLine("Account name: {0}\nDeposit amount: {1:c}\nAccount balance: {2:c}", _account.Name, _amount, _currBalance + _amount);
             }
             else
             {
-                Console.WriteLine("\n***** Deposit failed *****\n");
+                Console.WriteLine("\t***** Account Summary *****\n");
+                Console.WriteLine("{0}  ** Deposit failed **\n", DateStamp.ToString());
                 Console.WriteLine(_account.ToString());
             }
         }
