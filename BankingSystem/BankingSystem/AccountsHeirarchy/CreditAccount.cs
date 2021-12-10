b6c6a457e9bf0496444a94a066f438af3af37252
Sikander Sandhu <sikandersandhu82@gmail.com>
2021-12-10 18:17:50 +1100

    Update CreditAccount inheriting from Account class
    This class specializes the base abstract account class and has the
    following updates:
    
    - contructor now initializes account number instead of account name. It
      call the base constructor to initialzie balance and account number.
    
    Implements the abstract methods:
    
    - override bool Deposit(decimal? amount = null)
    
    - override bool Withdraw(decimal amount)
    
    - override string ToString()

---
+++ BankingSystem/BankingSystem/AccountsHeirarchy/CreditAccount.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Xml;
+
+namespace BankingSystem
+{
+    /// <summary>
+    /// Specialized class CreditAccount inherits from abstract calss Account
+    /// implements abstract deposit and withdraw methods
+    /// </summary>
+    [DataContract]
+    class CreditAccount: Account
+    {
+        [DataMember]
+        public int Limit { get; set; }
+
+        // constructor: initializes base cosntructor and limit
+        public CreditAccount(decimal balance, long accNum, int limit): base(balance, accNum)
+        {
+            Limit = -limit;
+        }
+
+        // credit account desposit(payment) and withdraw(purchase) functionality
+        /// <summary>
+        /// Deposits the payment amount into the account and returns the 
+        /// true/false success status for the payment.
+        /// </summary>
+        /// <param name="amount">the <c>payment</c> to be added to the account</param>
+        /// <returns>true/false payment success status</returns>
+        public override bool Deposit(decimal? amount = null)
+        {
+            if (amount.HasValue && amount > 0)
+            {
+                _balance += (decimal)amount;
+                return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// Withdraws the purchase amount from the account and return the 
+        /// true/false success status for the purchase.
+        /// </summary>
+        /// <param name="amount">the <c> purchase amount</c> to be withdrawn from the account</param>
+        /// <returns>true/false success status of the purchase</returns>
+        public override bool Withdraw(decimal amount)
+        {
+            if (Balance - amount >= Limit)
+            {
+                _balance -= amount;
+                return true;
+            }
+            return false;
+        }
+
+        // returns base ToString() method with Limit details and CreditAccount summary title
+        public override string ToString()
+        {
+            return string.Format("\t\n***** Credit Account Summary *****\n\n" + base.ToString()+"{0}\nLimit: {1:c}", Balance == 0 ? "" : (Balance < 0 ? "Cr" : "Dr"), Math.Abs(Limit));
+        }
+    }
+}
