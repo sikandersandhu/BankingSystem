b09cbbefbf70736526599a2c33be24a715a8eb74
Sikander Sandhu <sikandersandhu82@gmail.com>
2021-12-10 17:54:20 +1100

    Add observer pattern for mobile notifications
    This interface will be implemented as part of the observer pattern by
    the Bank class for managing the subscribers list
    and a method to send notifications to the observers.

---
+++ BankingSystem/BankingSystem/Interfaces/IPublisher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem
+{
+    /// <summary>
+    /// Observer pattern: interface to be implemented by the 
+    /// publisher class.
+    /// It enables subrscriber to be attached and detached from the
+    /// subscribers list and notify them when an event occurs
+    /// </summary>
+    public interface IPublisher
+    {
+        /// <summary>
+        /// Attaches observers to the subscriber list
+        /// </summary>
+        /// <param name="observer">the subscriber to be added</param>
+        void Attach(AccountHolder observer);
+        /// <summary>
+        /// Detaches observers from the subscribers list
+        /// </summary>
+        /// <param name="observer">the subscriber to be detached</param>
+        void Detach(AccountHolder observer);
+        /// <summary>
+        /// Notifies all subscribers about the event they have subscribed
+        /// for, when it occurs.
+        /// </summary>
+        void Notify();
+    }
+}
