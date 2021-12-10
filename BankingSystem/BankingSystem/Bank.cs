4d0bc3ee540980ba454c7116694e13c52d56eecc
Sikander Sandhu <sikandersandhu82@gmail.com>
2021-12-10 16:48:05 +1100

    Add new features to the bank
    This class has been updated and now implements IPublisher interface, associated
    with the observer pattern and IListAccess interfaces which provide
    functionality for managing subscriptions in the subscribers list and
    managing accounts in the account list respectively.
    
    - A new list of subscribers manages subscriptions for account
      notifications.
    
    - A new list of account numbers, manages the list of account numbers
      currently held in the bank. Two methods are provided:
    
      - RemAccNum(long accNum); to remove accounts.
    
      - long GenAccNum(); to generate a new account number and to ensure the
        account number is unique.
    
    For managing accounts list IListAccess interface is implemented which
    provides the following generic methods:
    
    - void Add<T>(T toAdd)
    
    - void Rem<T>(T toRem)
    
    - T Find<T>(params object[] list) where T: class
    
    Updated functionality:
    
    - void ExecuteTransaction(Transaction transaction); updated to send
      notifications to subscribers for account notifications.
    
    - void RollbackTransaction(Transaction transaction); updated to send
      notifications to subscribers for account notifications.
    
    New functionality:
    
    - Account FindTransacAcc(Transaction transaction, out Account toAcc);
      this method returns the reference of the account associated with the
      current transaction to determine if the account holder associated with
      the transaction is an observer for notifications.
    
    As part of the observer pattern and implementation of the ISubscriber
    interface, the following methods help manage subscribers in the
    subscribers list:
    
    - void Attach(AccountHolder observer)
    
    - void Detach(AccountHolder observer)
    
    - AccountHolder FindObserver(long accNum, string fName, string lName)
    
    - AccountHolder FindObserver(long accNum)
    
    - void Notify()

--- BankingSystem/BankingSystem/Bank.cs
+++ BankingSystem/BankingSystem/Bank.cs
@@ -6,88 +6,139 @@ using System.Threading.Tasks;
 using System.Runtime.Serialization;
 using System.Xml;
 
-namespace BankingSystem_Iteration5_AddAccountTypes
-{   /// <summary>
-/// The instance from this class has two roles. 
-/// First, to add new accounts to the 
-/// the List of accounts and to find out if an account already exists in the list or not.
-/// The methods either returns the found account reference or a null reference if
-/// account not found.
-/// Second, The overriden ExecuteTransaction method, calls methods from the DepositTransaction,
+namespace BankingSystem
+{   
+    /// <summary>
+/// The bank consits a list of accounts, transactions, subscriptions, account numbers and methods with
+/// relevant functionality for the respective lists.
+/// The overriden ExecuteTransaction method, calls methods from the DepositTransaction,
 /// WithdrawTransaction and TransferTransaction classes to execute transaction.
+/// The overriden RollbackTransaction method, calls methods from the DepositTransaction,
+/// WithdrawTransaction and TransferTransaction classes to rollback transaction.
+/// Also provides a method to print transaction history
 /// </summary>
     [DataContract]
     [KnownType(typeof(List<Account>))]
     [KnownType(typeof(List<Transaction>))]
-    class Bank
+    [KnownType(typeof(List<AccountHolder>))]
+    [KnownType(typeof(List<long>))]
+    class Bank : IPublisher, IListAccess
     {
-        /// <summary>
-        /// this field is a list of type Account, into which accounts will be added
-        /// </summary>
-        [DataMember(Name = "List_of_Accounts")]
-        private List<Account> _accounts;
-        [DataMember(Name = "List_of_Transactions")]
+        [DataMember(Name ="List_of_subscribers")]
+        public List<AccountHolder> Subscribers { get; private set; }
+
+        public Transaction Transaction { get; private set; }
+
+        public Account Account { get; private set; }
+        public Account ToAccount { get; private set; }
+
+        [DataMember(Name = "List_of_accounts")]
+        public List<Account> Accounts { get; private set; }
+
+        [DataMember(Name = "List_of_transactions")]
         private List<Transaction> _transactions;
+
         /// <summary>
-        /// This constructor initializes a new list of Account type
+        /// List of exisiting account numbers to ensure there are no duplicates
         /// </summary>
+        [DataMember(Name = "List_of_account_numbers")]
+        public List<long> AccNumbers { get; private set; }
+
+        // contrcutor: initializes list of accounts, transactions, account numbers and subscribers
         public Bank()
         {
-            _accounts = new List<Account>();
+            Accounts = new List<Account>();
             _transactions = new List<Transaction>();
+            AccNumbers = new List<long>();
+            Subscribers = new List<AccountHolder>();
         }
-        /// <summary>
-        /// Adds a new account to the list
-        /// </summary>
-        /// <param name="account">The account to be added</param>
-        /// <exception cref="InvalidOperationException">if the account with the name already exists</exception>
-        public void AddAccount(Account account)
-        {
-            // if the list of account is not empty
-            if(_accounts != null)
-            {
-                // Check each account
-                foreach(Account acc in _accounts)
-                {
-                    // if the account already exist throw an exception
-                    if (acc.Name == account.Name) throw new InvalidOperationException("Could not create account. An account with this name already exists.");
-                }
-            }
-            // if the account does not exist, add the account to the list.
-            _accounts.Add(account);
-        }
-        /// <summary>
-        /// Using the provided account name, searches the list of accounts
-        /// and returns the result
-        /// </summary>
-        /// <param name="name">name of the account to search for</param>
-        /// <returns>account reference if account found or null reference if account not found</returns>
-        public Account GetAccount(string name)
-        {
-            //account with a null reference to assign the search result to
-            Account account = null;
 
-            // If the list is not empty
-            if (_accounts != null)
-            {   
-                // look through the list
-                foreach(Account acc in _accounts)
+        /// <summary>
+        /// Generates random account numbers and adds a unique number 
+        /// to the list of account numbers
+        /// </summary>
+        /// <returns>a unique 8 to 9 digit account number</returns>
+        public long GenAccNum()
+        {
+            long accNum;
+            // Generates and saves eight digit random numeric account number
+            accNum = (DateTime.Now.Ticks / 10) % 1000000000;
+
+            // If list is empty, adds account number to list
+            if (AccNumbers.Count == 0) AccNumbers.Add(accNum);
+            else
+            {
+                // while list of account numbers already contains the
+                // account number continues to generate new numbers
+                // till a unique account number is found
+                while (AccNumbers.Contains(accNum))
                 {
-                    // If the account exists, assign it to account variable
-                    if (acc.Name == name) account = acc;
+                    accNum = (DateTime.Now.Ticks / 10) % 1000000000;
                 }
+                // and adds the number to the list
+                AccNumbers.Add(accNum);
             }
-            // if account found, returns account reference, else account with null reference
-            return account;            
+            // returns new accout number for the account
+            return accNum;
         }
         /// <summary>
-        /// Calls the Execute method to transfer funds, using the TransferTransaction object.
+        /// removes account number from the list of account numbers
         /// </summary>
-        /// <param name="deposit">Initialized TransferTransaction object, to be used to execute transfer</param>
+        /// <param name="accNum">the account number to be removed</param>
+        public void RemAccNum(long accNum)
+        {
+            if (AccNumbers == null) throw new NullReferenceException("\n***** NullReference: Account numbers list not found *****\n");
+            if (!AccNumbers.Contains(accNum)) throw new InvalidOperationException("\n***** InvalidOperation: Account number not found. *****\n");
+
+            AccNumbers.Remove(accNum);
+            Console.WriteLine("\n***** Success: Account number removed *****\n");
+        }
+
+        // IListAccess interface methods to access the list of accounts
+        public void Add<T>(T toAdd)
+        {
+            if (toAdd == null) throw new NullReferenceException("\n***** NullReference: Reference for the account is missing*****\n");
+            if (Accounts == null) throw new NullReferenceException("\n***** NullReference: Accounts list not found *****\n");
+            if (Accounts.Contains(toAdd as Account)) throw new InvalidOperationException("\n***** InvalidOperation: An account with the given details already exists *****\n");
+
+            Accounts.Add(toAdd as Account);
+            Console.WriteLine("\n***** Success: {0} added *****\n", toAdd.GetType().Name.ToString());
+        }
+        public void Rem<T>(T toRem)
+        {
+            if (toRem == null) throw new NullReferenceException("\n***** NullReference: Reference for the account is missing*****\n");
+            if (Accounts == null) throw new NullReferenceException("\n***** NullReference: Accounts list not found *****\n");
+            if (!Accounts.Contains(toRem as Account)) throw new InvalidOperationException("\n***** InvalidOperation: An account with the given details does not exists *****\n");
+
+            Accounts.Remove(toRem as Account);
+            Console.WriteLine("\n***** Success: {0} removed *****\n", toRem.GetType().Name);
+        }
+        public T Find<T>(params object[] list) where T: class
+        {
+            var accNum = list[0];
+            foreach (object obj in list) if (obj == null) throw new ArgumentException("\n***** ArgumnetException: argument can not be null *****\n");
+            if (Accounts == null) throw new NullReferenceException("\n***** NullReference: Accounts list empty *****\n");
+
+            Account toFind = null;
+
+            foreach (Account acc in Accounts) if (acc.AccNum == (long)accNum) toFind = acc;
+
+            return toFind as T;
+        }
+
+        // Execute and Rollback transaction functions that call polymorphically call
+        // DepositTransaction, WithdrawTransaction and TransferTransaction methods depending
+        // on the the type of transaction object provided as the parameter.
         public void ExecuteTransaction(Transaction transaction)
         {
             // Execute the transaction
             transaction.Execute();
+
+            Transaction = transaction;
+            Account = FindTransacAcc(Transaction, out Account toAcc);
+            ToAccount = toAcc;
+            this.Notify();
+
             // Adds transactions to the list of transactions
             _transactions.Add(transaction);        
         }
@@ -95,9 +146,20 @@ namespace BankingSystem_Iteration5_AddAccountTypes
         {
             // Execute the transaction
             transaction.Rollback();
+
+            Transaction = transaction;
+            Account = FindTransacAcc(Transaction, out Account toAcc);
+            ToAccount = toAcc;
+
+            this.Notify();
+
             // Adds transactions to the list of transactions
             _transactions.Add(transaction);          
         }
+
+        /// <summary>
+        /// Prints transaction history
+        /// </summary>
         public void PrintTransactionHistory()
         {
             if (_transactions != null)
@@ -106,6 +168,96 @@ namespace BankingSystem_Iteration5_AddAccountTypes
                 for (int i = _transactions.Count - 1; i >= 0; i--) _transactions[i].Print();
             }
             else Console.WriteLine("There are currently no transactions to print.");
+        }
+
+        // IPublisher interface Attach, Detach, Notify methods and two other methods to 
+        // add and remove subscribers from the list and notify them of the transaction they are observing.
+        public static Account FindTransacAcc(Transaction transaction, out Account toAcc)
+        {
+            toAcc = null;
+            Account acc = null;
+
+            // Variables to save account type
+            Type type = transaction.GetType();         // What type of transaction is it?
+
+            if (type == typeof(DepositTransaction))    // if it is DepositTransaction
+            {
+               acc  = (transaction as DepositTransaction).Account; // assign account ref
+            }
+            if (type == typeof(WithdrawTransaction))    // if it is WithdrawTransaction
+            {
+                acc = (transaction as WithdrawTransaction).Account; // assign account ref
+            }
+            if (type == typeof(TransferTransaction))    // if it is TransferTransaction
+            {
+                acc = (transaction as TransferTransaction).FromAccount; // assign account ref
+                toAcc = (transaction as TransferTransaction).ToAccount; // assign account ref
+            }
+            
+            return acc; // Return current account ref
+        }
+        public void Attach(AccountHolder observer)
+        {
+            if (observer == null) throw new NullReferenceException("\n***** Fail: NullReference - Subscription information is missing *****\n");
+            if (Subscribers == null) throw new NullReferenceException("\n***** NullReference: Subscribers list not found *****\n");
+            if (Subscribers.Contains(observer)) throw new InvalidOperationException("\n***** Fail: A subscription already exists for the account holder *****\n");
+            
+            Subscribers.Add(observer);
+            Console.WriteLine("\n***** Success: Subscriber added *****\n");
+        }
+        // Detaches observers from the subscribers list
+        public void Detach(AccountHolder observer)
+        {
+            if (observer == null) throw new NullReferenceException("\n***** Fail: NullReference - Subscription information is missing *****\n");
+            if (Subscribers == null) throw new NullReferenceException("\n***** NullReference: Subscribers list not found *****\n");
+            if (!Subscribers.Contains(observer)) throw new InvalidOperationException("\n***** InvalidOperation: There are no current subscriptions for the account holder. *****\n");
+            
+            Subscribers.Remove(observer);
+            Console.WriteLine("\n***** Success: subscriber removed *****\n");
+        }
+        public AccountHolder FindObserver(long accNum, string fName, string lName)
+        {
+            if ( fName == null || lName == null ) throw new NullReferenceException("\n***** Infomation missing: Both first and last names are required. *****\n");
+
+            AccountHolder _ = null;
+
+            if (Subscribers.Count > 0)
+            {
+                foreach (AccountHolder subscriber in Subscribers)
+                {
+                    if (subscriber.AccNum == accNum && subscriber.FName == fName && subscriber.LName == lName) _ = subscriber;
+                    else throw new InvalidOperationException("\n***** A subscription with the provided details could not be found *****\n");
+                }                   
+            }
+            return _;
+        }
+        public AccountHolder FindObserver(long accNum)
+        {
+            AccountHolder _ = null;
+
+            if (Subscribers.Count > 0)
+            {
+                foreach (AccountHolder subscriber in Subscribers)
+                {
+                    if (subscriber.AccNum == accNum) _ = subscriber;
+                    else throw new InvalidOperationException("\n***** A subscription with the provided details could not be found *****\n");
+                }
+            }
+            return _;
+        }
+
+        // Notifies all observers about the event of interest
+        // as it occurs.
+        public void Notify()
+        {
+            foreach (AccountHolder accHolder in Subscribers) 
+                if (accHolder.AccNum == Account.AccNum)
+                {
+                    Console.WriteLine("\n***** Subject: Notifying subscribers *****\n");
+                    accHolder.Update(this);
+                    if (ToAccount != null)
+                        if (accHolder.AccNum == ToAccount.AccNum) accHolder.Update(this);
+                }
         }
     }
 }
