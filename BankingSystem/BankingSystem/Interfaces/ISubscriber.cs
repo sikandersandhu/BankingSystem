697cb8da37f64d6c9ca0b772ae3b7fe80b2de08f
Sikander Sandhu <sikandersandhu82@gmail.com>
2021-12-10 17:58:15 +1100

    Add interface for observer pattern
    This interface will be implemented by the AccountHolder class which
    includes an Update method that send updates to the account holder that
    is observing for notifications.

---
+++ BankingSystem/BankingSystem/Interfaces/ISubscriber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem
+{
+    /// <summary>
+    /// Observer pattern: interface to be implemented by the 
+    /// subscriber class, that will update subscriber about
+    /// an event when it occurs.
+    /// </summary>
+    public interface ISubscriber
+    {
+        /// <summary>
+        /// Publishes notification about an event, when it occurs, 
+        /// which the observer has subscribed to.
+        /// </summary>
+        /// <param name="subject">The object notifying about the event
+        /// and containing information that will be published</param>
+        void Update(IPublisher subject);
+    }
+}
