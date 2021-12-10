14c0942832ad5a0189eb379c33a067e9448d7daf
Sikander Sandhu <sikandersandhu82@gmail.com>
2021-12-10 16:31:44 +1100

    Add AccountHolder class
    This class initializes an account holder that is to be added to an
    account.
    
    It implements ISubscriber interface. If an account holder subscribes for
    account notification, the following method sends a mobile notification to
    the account holder:
    
    - void Update(IPublisher subject)

---
+++ BankingSystem/BankingSystem/AccountHolder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Xml;
+
+namespace BankingSystem
+{
+    /// <summary>
+    /// Account holder to be added to the account
+    /// </summary>
+    [DataContract(Name ="Account_holder")]
+    public class AccountHolder: ISubscriber
+    {
+        [DataMember(Name ="Account_number")]
+        public readonly long AccNum;
+        [DataMember(Name = "First name")]
+        public string FName { get; private set; }
+        [DataMember(Name = "Last_name")]
+        public string LName { get; private set; }
+
+        // constructor: initializes the account holder with first, second name
+        // and account number
+        public AccountHolder(string fName, string lName, long accNum)
+        {
+            FName = fName;
+            LName = lName;
+            AccNum = accNum;
+        }
+
+        // returns string representation of the account holder
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", FName, LName);
+        }
+
+        // updates account holder about an event they have subscribed for
+        public void Update(IPublisher subject)
+        {
+            Console.WriteLine("\n***** Mobile device notification: *****\n");
+            (subject as Bank).Transaction.Print();
+        }
+    }
+}
