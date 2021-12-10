fef3d4138441c4f38ec97e73908b054b8796435c
Sikander Sandhu <sikandersandhu82@gmail.com>
2021-12-10 11:53:36 +1100

    Update Account class to create account heirarchy
    - The Account class is now an abstract class with a protected
      constructor. It can now only be instanciated through its specialized
      classes, namely CreditAccount, SavingsAccount, TermDepositAccount.
    
    Other updated functionality:
    
    - abstract bool Deposit(decimal? amount = null); previous deposit method
      removed. New abstract method must be overriden in specialized classes.
    
    - abstract bool Withdraw(decimal amount); previous withdraw method
      removed. New abstract method must be overriden in specialized classes.
    
    - override string ToString(); updated to provide account summary in a format
      suited for the respective sepcialized classes.

--- BankingSystem/BankingSystem/Account.cs
+++ BankingSystem/BankingSystem/Account.cs
@@ -6,31 +6,32 @@ using System.Threading.Tasks;
 using System.Runtime.Serialization;
 using System.Xml;
 
-namespace BankingSystem_Iteration4_serializing
+namespace BankingSystem_Iteration5_AddAccountTypes
 {
     /// <summary>
     /// This is the core <c>Account</c> class that forms part of the banking system.
     /// It includes methods that deposit and withdraw funds and print account summary.
     /// </summary>
     [DataContract]
-    class Account
+    [KnownType(typeof(SavingsAccount))]
+    [KnownType(typeof(TermDepositAccount))]
+    [KnownType(typeof(CreditAccount))]
+    abstract class Account
     {
         /// <summary>
         /// The field to be initialized with the initial account balance
         /// </summary>
         [DataMember(Name = "Balance")]
-        private decimal _balance;
+        protected decimal _balance;
         public decimal Balance
         {
-            get { return _balance; }
+            get => _balance;
         }
-
         /// <summary>
         /// The field to be initialized with the account name
         /// </summary>
         [DataMember(Name = "Account_Name")]
         private string _name;
-
         /// <summary>
         /// Public property to read account name
         /// </summary>
@@ -40,64 +41,24 @@ namespace BankingSystem_Iteration4_serializing
             {
                 return _name;
             }
-        }
-        
+        }       
         /// <summary>
         /// This constructor initializes the account name and balance fields.
         /// </summary>
         /// <param name="balance">the initial accounts balance.</param>
         /// <param name="name">name of the account</param>
-        public Account(decimal balance, string name)
+        protected Account(decimal balance, string name)
         {
             _balance = balance;
             _name = name;
         }
-
-        /// <summary>
-        /// Deposits the amount into the account and returns the 
-        /// true/false success status for the deposit.
-        /// </summary>
-        /// <param name="amount">the <c>amount</c> to be added to the account</param>
-        /// <returns>true/false deposit success status</returns>
-        /// <exception cref="InvalidOperationException">if the amount to be deposited
-        /// is not greater than zero</exception>
-        public bool Deposit(decimal amount)
+        public Account(Account account)
         {
-            // if the amount is greater than zero
-            if (amount > 0)
-            {
-                // Deposit the funds
-                _balance += amount;
-                // return success status
-                return true;
-            }
-            // else return fail status
-            else return false;
+            _balance = account.Balance;
+            _name = account.Name;
         }
-
-        /// <summary>
-        /// Withdraws the amount from the account and return the 
-        /// true/false success status for the withdrawal.
-        /// </summary>
-        /// <param name="amount">the <c>amount</c> to be withdrawn from the account</param>
-        /// <returns>true/false success status for the withdrawal</returns>
-        /// <exception cref="InvalidOperationException">if the <c>amount</c> to be 
-        /// withdrawn is less than zero or greater than the current account <c>_balance</c>
-        /// </exception>
-        public bool Withdraw(decimal amount)
-        {
-            // if the amount is greater than zero and not greater than the balance
-            if (amount > 0 && amount <= _balance)
-            {
-                // withdraw the amount
-                _balance -= amount;
-                // return the success status
-                return true;
-            }
-            // else return fail status
-            else return false;
-        }
-
+        public abstract bool Deposit(decimal? amount = null);
+        public abstract bool Withdraw(decimal amount);      
         /// <summary>
         /// Provides the account summary as a string.
         /// </summary>
@@ -112,7 +73,7 @@ namespace BankingSystem_Iteration4_serializing
         /// </example></returns>
         public override string ToString()
         {
-            return string.Format("\t***** Account Summary *****\n\nAccount name: {0}\nAccount balance: {1:c}",_name, _balance);
+            return string.Format("Account name: {0}\nAccount balance: {1:c}",_name, Math.Abs(_balance));
         }
     }
 }
